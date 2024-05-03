6e12d6866d5dc822fff309b982a24017f1e909e4
omrawat23 <57662461+omrawat23@users.noreply.github.com>
2024-05-03 16:35:40 +0530

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,88 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/nLmjwBjj/removal-ai-18c5e8c6-674d-4c24-b422-bc0d08e0a96b-peakpx-5.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/sfTXmw6H/removal-ai-efd5ee34-3e1a-4242-8788-902cb0bd2a9b-yalamber-limbu-ds2zidnxwgk-unsplash.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/15x9J20H/removal-ai-400e5fb5-3cd4-4b2e-a174-063c888985e6-pat-kwon-ejtjetc8tps-unsplash.png",
+    title: "FALL COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/FRgPvkhZ/peakpx-2.jpg",
+    title: "",
+    cat:"women"
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/zBsRx0s2/peakpx-7.jpg",
+    title: "",
+    cat:"coat"
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/HxsMJrQ3/peakpx-6.jpg",
+    title: "",
+    cat:"jeans"
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://i.postimg.cc/VLnD5gV7/Screenshot-2024-04-14-054131.png",
+  },
+  {
+    id:2,
+    img:"https://i.postimg.cc/J4xHTBDr/Screenshot-2024-04-14-060059.png",
+  },
+  {
+    id:3,
+    img:"https://i.postimg.cc/zGPbNZF4/Screenshot-2024-04-14-060139.png",
+  },
+  {
+    id:4,
+    img:"https://i.postimg.cc/wjBynTLh/Screenshot-2024-04-14-060214.png",
+  },
+  {
+    id:5,
+    img:"https://i.postimg.cc/zGPbNZF4/Screenshot-2024-04-14-060139.png",
+  },
+  {
+    id:6,
+    img:"https://i.postimg.cc/zGPbNZF4/Screenshot-2024-04-14-060139.png",
+  },
+  {
+    id:7,
+    img:"https://i.postimg.cc/VLnD5gV7/Screenshot-2024-04-14-054131.png",
+  },
+  {
+    id:8,
+    img:"https://i.postimg.cc/J4xHTBDr/Screenshot-2024-04-14-060059.png",
+  },
+  {
+    id:9,
+    img:"https://i.postimg.cc/wjBynTLh/Screenshot-2024-04-14-060214.png",
+  },
+  {
+    id:10,
+    img:"https://i.postimg.cc/zGPbNZF4/Screenshot-2024-04-14-060139.png",
+  },
+  
+]
